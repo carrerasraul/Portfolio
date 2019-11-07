a36c7fd3cb01f2b32b0502a06cf41059a53770b8
carrerasraul <52898376+carrerasraul@users.noreply.github.com>
2019-11-07 00:45:32 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ document.getElementById('quizmodal').addEventListener('click', function() {
 document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'none';
 });
+
+
+//SMOOTH SCROLL//
+$('#aboutbtn').on('click', function() {
+    const quizmodal = $('#aboutbtn').position().top;
+
+    $('html, body').animate({
+        scrollTop: quizmodal
+    }, 900);
+})
